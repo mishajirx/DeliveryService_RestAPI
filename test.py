89c3db11b153c45e9dd30dac782894b97b1e9260
Mike <mishalolhopengok@gmail.com>
2021-03-21 20:01:30 +0300

    Change ports to 0.0.0.0

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import datetime
 import argparse
 import requests
 
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
+# HOST = '127.0.0.1'
 parser = argparse.ArgumentParser(  # объект обрабатывающий аргументы(как в функции)
     description="convert integers to decimal system")
 parser.add_argument('--clear', default='0', type=str, help='need to delete all data?(yes(1)/no(0))')
