18353019d0592fa7a535bf92a0afebc329c58961
Mike <mishalolhopengok@gmail.com>
2021-03-23 20:59:00 +0300

    Changed host to 0.0.0.0

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 x = 0
 
 
-@app.errorhandler(400)
-def bad_request(error, *args):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
+# @app.errorhandler(400)
+# def bad_request(error, *args):
+#     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 #
@@ -26,8 +26,8 @@ def hello():
 def main():
     db_session.global_init("db/couriers.db")
     app.register_blueprint(shop_api.blueprint)
-    app.run(port=8080)
-    # app.run(host='0.0.0.0', port=8080)
+    # app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 if __name__ == '__main__':
